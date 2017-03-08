cd0606b244a6e16bdf33bf1fae1fa3caad4749e4
Félix Archambault <fel.archambault@gmail.com>
2017-03-08 12:20:48 -0500

    feat: splitted main into two modes with each an included task file
    new structure, now when the var sql_path is passed it will iterate over
    .sql files under that path.
    
    not implemented yet, this is only changed the role structure
    
    Signed-off-by: Félix Archambault <fel.archambault@gmail.com>

--- tests/scripts/postgres/test_postgres_query_return_facts.sql
+++ tests/scripts/postgres/test_postgres_query_return_facts.sql
@@ -1,1 +1,1 @@
-SELECT date_time, table_name FROM etl_merged_tables order by date_time desc limit %(nb_results)s;
+SELECT date_time FROM etl_merged_tables order by date_time desc limit %(nb_results)s;
